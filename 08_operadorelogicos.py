170ade7383c0252ceb57781742e66c532c3e199e
Oscar Julio Silvera Cruz <ojsilvera@gmail.com>
2023-04-06 17:13:29 +0000

    Agregue operador logico or y un pequeño ejemplo

--- 08_operadorelogicos.py
+++ 08_operadorelogicos.py
@@ -11,3 +11,19 @@ stock = input('digite cant de unidades => ')
 stock = int(stock)
 
 print(stock >= 100 and stock <= 1000)
+
+#or
+print('*' * 10)
+print('OR')
+print('*' * 10)
+
+print('True and True =>', True or True)
+print('True and False =>', True or False)
+print('False and True =>', False or True)
+print('False and False =>', False or False)
+
+role = input('Digita rl rol => ')
+
+print(role == 'admin' or role == 'seller')
+
+
