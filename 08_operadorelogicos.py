5010eb1d01211323e8a2fc647464480fcd2d2e3e
Oscar Julio Silvera Cruz <ojsilvera@gmail.com>
2023-04-06 17:08:44 +0000

    Agregue el operador logico and

---
+++ 08_operadorelogicos.py
@@ -0,0 +1,13 @@
+#and
+print('AND')
+print('True and True =>', True and True)
+print('True and False =>', True and False)
+print('False and True =>', False and True)
+print('False and False =>', False and False)
+
+print(10 > 5 and 2 < 8)
+
+stock = input('digite cant de unidades => ')
+stock = int(stock)
+
+print(stock >= 100 and stock <= 1000)
