5e8d44ccb679d4f6aeec32f13d2c4cade708f48c
Oscar Julio Silvera Cruz <ojsilvera@gmail.com>
2023-04-11 16:00:36 +0000

    declaramos los imputs, la logica para evaluar el empate y una posible combinacion de jugadas, eleecion del pc fijo de momento

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 # juego piedra papel o tijera, del curso introduccion a python platzi
+
+# logica de programacion
+# se eleijen las opciones de cada jugador
+
+user_option = input('piedra, papel o tijera? => ')
+computer_option = 'piedra'
+
+#se evaluan las dos opciones
+if user_option == computer_option:
+  print('empate')
+elif user_option == 'piedra':
+  if computer_option == 'tijera':
+    print('tijera pierde con piedra')
+    print('user win')
+  else:
+    print('papel gana a piedra')
+    print('computer win')
+
+elif user_option == 'papel':
+  print('computer win')
