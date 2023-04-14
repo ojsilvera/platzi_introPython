e4687c2a7e460dc9579e9e72d6b53a47edd7d9a5
Oscar Silvera Cruz <ojsilvera@gmail.com>
2023-04-14 13:31:53 -0500

    posicions negativas, que incluyen desde una posicion negativa hasta la primera posicion negativa o ultima de la tupla

--- 12_slicing&indexing.py
+++ 12_slicing&indexing.py
@@ -17,6 +17,7 @@ text = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"]
 #print(text[2 : 6]) #(i : f-1)
 #nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1.5, 2.8]
 #print (nums [-9 : -3]) #(7, 8, 9)(i : f-1)
+#print (nums [-9 : ]) #(incluye todas las posiciones desde el -9 hasta la primera posicion negativa
 #print(text[5:]) #muestra desde una posicion estipulada hasta el fnal de la tupla
 #print(text[:]) #utiliza todos los valores de la tupla
 #print(text[1:9:3]) #un rango en pasos de 3
