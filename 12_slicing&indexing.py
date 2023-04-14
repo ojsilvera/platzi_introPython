6be7fe7edad9359336e761047c74bf3ac477bb1f
Oscar Silvera Cruz <ojsilvera@gmail.com>
2023-04-14 11:23:39 -0500

    practica de tuplas, posicionamiento positivo y negativo

--- 12_slicing&indexing.py
+++ 12_slicing&indexing.py
@@ -1,21 +1,27 @@
 #indexing
-text = "Ella sabe Python"
-print(text[0])
-print(text[1])
+#text = "Ella sabe Python"
+text = ["a", "b", "c", "d", "e", "f", "g", "h"]
+
+# print(text[0])
+# print(text[1])
+
 # print(text[999])
-size = len(text)
-print('size => ', size)
-print(text[size - 1])
-print(text[-1])
+# size = len(text)
+# print('size => ', size)
+# print(text[size - 1])
+# print(text[-1])
 
 # slicing
 
-print(text[0:5])
-print(text[10:16])
-print(text[:10])
-print(text[5:-1])
-print(text[5:])
-print(text[:])
-print(text[10:16:1])
-print(text[10:16:2])
-print(text[::2])
+# print(text[0:5])
+# print(text[10:16])
+# print(text[:10])
+print(text[2 : 6]) #(i : f-1)
+# print(text[5:])
+# print(text[:])
+# print(text[10:16:1])
+# print(text[10:16:2])
+# print(text[::2])
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1.5, 2.8]
+print (nums [-9 : -3]) #(7, 8, 9)(i : f-1)
