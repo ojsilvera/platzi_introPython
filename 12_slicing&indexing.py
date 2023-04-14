c7188444b6f445f62a336998ef2008d1adbb2b0b
Oscar Silvera Cruz <ojsilvera@gmail.com>
2023-04-14 13:24:55 -0500

    agregando comentarios a las diferentes formas de establecer el recorrido de un rango

--- 12_slicing&indexing.py
+++ 12_slicing&indexing.py
@@ -1,27 +1,25 @@
-#indexing
 #text = "Ella sabe Python"
-text = ["a", "b", "c", "d", "e", "f", "g", "h"]
+text = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"]
 
-# print(text[0])
-# print(text[1])
+# slicing and indexing-----------------
+#print(text[0])
+#print(text[1])
 
-# print(text[999])
-# size = len(text)
-# print('size => ', size)
-# print(text[size - 1])
-# print(text[-1])
+#print(text[999])
+#size = len(text)
+#print('size => ', size)
+#print(text[size - 1])
+#print(text[-1])
 
-# slicing
+#print(text[0:5])
+#print(text[10:16])
+#print(text[:10]) #muestra los valores desde la posicion inicial hasta la posicion maxima estipulada
+#print(text[2 : 6]) #(i : f-1)
+#nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1.5, 2.8]
+#print (nums [-9 : -3]) #(7, 8, 9)(i : f-1)
+#print(text[5:]) #muestra desde una posicion estipulada hasta el fnal de la tupla
+#print(text[:]) #utiliza todos los valores de la tupla
+#print(text[1:9:3]) #un rango en pasos de 3
+#print(text[10:16:2]) #un rango en pasos de 2 desde la posicion inicial y final estipuladas
+#print(text[::2]) #asume todo el rango en pasos de dos
 
-# print(text[0:5])
-# print(text[10:16])
-# print(text[:10])
-print(text[2 : 6]) #(i : f-1)
-# print(text[5:])
-# print(text[:])
-# print(text[10:16:1])
-# print(text[10:16:2])
-# print(text[::2])
-
-nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1.5, 2.8]
-print (nums [-9 : -3]) #(7, 8, 9)(i : f-1)
