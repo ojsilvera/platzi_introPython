31f6ac60ffdd418fbd9774bba0f7094a23387f4f
Oscar Silvera Cruz <ojsilvera@gmail.com>
2023-04-14 15:32:28 -0500

    documentando los metodos de listas o tuplas

--- 14_metodosListas.py
+++ 14_metodosListas.py
@@ -2,24 +2,25 @@
 
 numbers = [1, 2 , 3 , 4 , 5]
 print(numbers[1])
+
 numbers[-1] = 10
 print(numbers)
 
-numbers.append(700)
+numbers.append(700) #agrega un nuevo elemento al final de la tupla
 print(numbers)
 
-numbers.insert(0, 'hola')
+numbers.insert(0, 'hola') #lista.insert(posicion, elementoHaInsertar)
 print(numbers)
 
 numbers.insert(3, 'change')
 print(numbers)
 
 tasks = ['todo 1', 'todo 2', 'todo 3']
-new_list = numbers + tasks
+new_list = numbers + tasks #une las tuplas 
 print(new_list)
 
-index = new_list.index('todo 2')
-new_list[index] = 'todo changed'
+index = new_list.index('todo 2') #nos muestra la posicion de un elemento de la tupla
+new_list[index] = 'todo changed' #reemplaza el elemento que se encuentra en la posicion almacenada en index
 print(new_list)
 
 new_list.remove('todo 1')
